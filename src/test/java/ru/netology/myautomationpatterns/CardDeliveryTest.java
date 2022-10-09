3207615acca590094d3ce446c5916baedc75e648
Sergei US <ustimenko.sv@gmail.com>
2022-10-09 14:16:10 +0300

    Initial commit

--- src/test/java/ru/netology/myautomationpatterns/CardDeliveryTest.java
+++ src/test/java/ru/netology/myautomationpatterns/CardDeliveryTest.java
@@ -1,8 +1,8 @@
 package ru.netology.myautomationpatterns;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -10,6 +10,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
